8f89744e6f123a583261b601e6887d7c5d0c31a6
isegoryachev <isegoryachev@avito.ru>
2025-04-07 11:53:53 +0300

    some fixes

--- internal/layers/storage/gorm/models/tag.go
+++ internal/layers/storage/gorm/models/tag.go
@@ -11,6 +11,3 @@ type Tag struct {
 	ID          uuid.UUID `gorm:"primary_key;type:uuid"`
 	TagsSpaceID uuid.UUID `gorm:"type:uuid;not null"`
 }
-
-// TODO fix linters on tests
-// TODO check if i can import medicine/internal/tooling/test
